55f54811fad39fad91eb1b435d28b32c9b3c20e4
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-09-27 16:31:12 +0200

    1st change

---
+++ UserMaintenence/UserMaintenence/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenence.Entities
+{
+    class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+    }
+}
