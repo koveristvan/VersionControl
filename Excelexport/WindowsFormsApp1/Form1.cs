f14e2964332fb106308e9e36467c256d96552b5d
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-10-02 21:23:53 +0200

    c

--- Excelexport/WindowsFormsApp1/Form1.cs
+++ Excelexport/WindowsFormsApp1/Form1.cs
@@ -78,6 +78,36 @@ namespace WindowsFormsApp1
                  "Ár (mFt)",
                  "Négyzetméter ár (Ft/m2)"
             };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            }
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+            
+            
         }
-    }
+        
+}
 }
