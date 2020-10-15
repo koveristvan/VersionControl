876dec3afbc77b9abf16f426c85408c671e862e6
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-10-15 21:25:57 +0200

    Harmadikfeladatután

--- Webszolg/Webszolg/Form1.cs
+++ Webszolg/Webszolg/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Webszolg.MnbServiceReference;
 
 namespace Webszolg
 {
@@ -15,6 +17,17 @@ namespace Webszolg
         public Form1()
         {
             InitializeComponent();
+            MNBArfolyamServiceSoapClient mnbservice = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbservice.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
