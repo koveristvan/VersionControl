7225e66a062a35c0b9754336f66676b985e40c30
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-10-15 22:21:09 +0200

    4.feladat

--- Webszolg/Webszolg/Entities/RateData.cs
+++ Webszolg/Webszolg/Entities/RateData.cs
@@ -8,5 +8,8 @@ namespace Webszolg.Entities
 {
     class RateData
     {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
     }
 }
--- Webszolg/Webszolg/Form1.cs
+++ Webszolg/Webszolg/Form1.cs
@@ -8,15 +8,27 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Webszolg.Entities;
 using Webszolg.MnbServiceReference;
 
 namespace Webszolg
 {
     public partial class Form1 : Form
     {
+        RichTextBox rtb1 = new RichTextBox();
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+            getexchangerates();
+            xmldata();
+        }
+
+        public void getexchangerates()
+        {
             MNBArfolyamServiceSoapClient mnbservice = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
             {
@@ -28,6 +40,20 @@ namespace Webszolg
             var response = mnbservice.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+            rtb1.Text = result;
+     
         }
+
+
+        public void xmldata()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(rtb1.Text.ToString());
+
+        }
+
+           
+    
     }
 }
