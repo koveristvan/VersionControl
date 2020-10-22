92735523aac0b85280a52380d0dc956dd110a51b
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-10-22 09:36:58 +0200

    elsomentes

---
+++ Mikroszim/Mikroszim/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszim.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double DeathProb { get; set; }
+    }
+}
---
+++ Mikroszim/Mikroszim/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszim.Entities
+{
+    class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
