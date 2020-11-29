f75417f2f75a3a7af94bebaacbd28cd295d58dfa
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-11-29 18:01:21 +0100

    adatbazis kesz_proba2

---
+++ Beadando__Z86RKF/Beadando__Z86RKF/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beadando__Z86RKF
+{
+    public partial class Form1 : Form
+    {
+
+        ReaEstateEntities context = new ReaEstateEntities();
+        public Form1()
+        {
+            InitializeComponent();
+
+            context.Realestate.Load();
+
+
+        }
+    }
+}
